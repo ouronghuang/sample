1e1acc3e7d631931956b87b4fdb33e34a5974c59
11dom-orh <512574561@qq.com>
2017-12-14 09:11:57 +0000

    Format code

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -1,20 +1,18 @@
 <!DOCTYPE html>
 <html>
-  <head>
+<head>
     <title>@yield('title', 'Sample App') - Laravel 入门教程</title>
     <link rel="stylesheet" href="/css/app.css">
-  </head>
-  <body>
-    @include('layouts._header')
-
-    <div class="container">
-        <div class="col-md-offset-1 col-md-10">
-          @include('shared._messages')
-          @yield('content')
-          @include('layouts._footer')
-        </div>
+</head>
+<body>
+@include('layouts._header')
+<div class="container">
+    <div class="col-md-offset-1 col-md-10">
+        @include('shared._messages')
+        @yield('content')
+        @include('layouts._footer')
     </div>
-
-    <script src="/js/app.js"></script>
-  </body>
+</div>
+<script src="/js/app.js"></script>
+</body>
 </html>
