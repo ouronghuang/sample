de60b1af7911aef9d4bcbcfa1896347abc79c256
ourh <512574561@qq.com>
2017-12-14 11:16:56 +0000

    Remove test

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -11,7 +11,7 @@ class UsersController extends Controller
     public function __construct()
     {
         $this->middleware('auth', [
-            'except' => ['show', 'create', 'store', 'index',]
+            'except' => ['show', 'create', 'store', 'index']
         ]);
 
         $this->middleware('guest', [
