9ec20e1df867d9ccac3fcbb634f2f67d11c8588b
11dom-orh <512574561@qq.com>
2017-12-18 09:17:19 +0000

    Update app info

--- resources/views/static_pages/home.blade.php
+++ resources/views/static_pages/home.blade.php
@@ -21,10 +21,7 @@
         </div>
     @else
         <div class="jumbotron">
-            <h1>Hello Laravel</h1>
-            <p class="lead">
-                你现在所看到的是 <a href="https://fsdhub.com/books/laravel-essential-training-5.1">Laravel 入门教程</a> 的示例项目主页。
-            </p>
+            <h1>Hello</h1>
             <p>
                 一切，将从这里开始。
             </p>
